eb28b731826ab9025b5e3daaeab0083afc97bb9f
rasterradio <funnyguy150@gmail.com>
2018-10-19 17:37:46 -0400

    poop shoot

--- Watch Jam/Assets/Scripts/PlayerPlatformerController.cs
+++ Watch Jam/Assets/Scripts/PlayerPlatformerController.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerPlatformerController : PhysicsObject
 {
-
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    AudioSource audioData;
+
+    /*public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;*/
+
     private SpriteRenderer spriteRenderer;
-    private Animator animator;
+    //private Animator animator;
 
     // Use this for initialization
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
+        audioData = GetComponent<AudioSource>();
     }
 
     protected override void ComputeVelocity()
@@ -35,6 +47,16 @@ public class PlayerPlatformerController : PhysicsObject
                 velocity.y = velocity.y * 0.5f;
             }
         }
+        /*if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }*/
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
 
         bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
         if (flipSprite)
@@ -42,9 +64,23 @@ public class PlayerPlatformerController : PhysicsObject
             spriteRenderer.flipX = !spriteRenderer.flipX;
         }
 
-        animator.SetBool("grounded", grounded);
-        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        //animator.SetBool("grounded", grounded);
+        //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
         targetVelocity = move * maxSpeed;
+    }
+
+    void shoot()
+    {
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        //audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+        Destroy(bullet, 2.0f);
     }
 }
