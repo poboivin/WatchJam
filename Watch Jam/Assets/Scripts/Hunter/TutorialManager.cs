42dc8e7b98971822fa6676ebd918f5ef7450d6ae
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2018-10-31 17:03:03 -0400

    Added The opening Tutorial

---
+++ Watch Jam/Assets/Scripts/Hunter/TutorialManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour {
+
+    public static int PlayersReadied;
+    public static int PlayersPlaying;
+    public static bool Active;
+
+    // Use this for initialization
+    void Start () {
+        //makes sure all the variables are reset
+        PlayersReadied = 0;
+        PlayersPlaying = 0;
+        Active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+  
+	}
+
+    //if the amount of players who are readied is >= the amount playing start the game
+    public void LoadLevel()
+    {
+       
+        if(PlayersReadied>= PlayersPlaying)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
