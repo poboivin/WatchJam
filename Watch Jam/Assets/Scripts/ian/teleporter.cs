a9d8f561e07165adc18bc723270f7fb08c0c5093
rasterradio <funnyguy150@gmail.com>
2018-11-01 15:03:18 -0400

    Sounds and fixes

--- Watch Jam/Assets/Scripts/ian/teleporter.cs
+++ Watch Jam/Assets/Scripts/ian/teleporter.cs
@@ -7,16 +7,8 @@ public class teleporter : MonoBehaviour {
     public teleporter Destination;
     public Transform Exit;
     public float TeleportOffset = 1;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AudioClip clip;
+    public AudioSource source;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,20 +16,22 @@ public class teleporter : MonoBehaviour {
 
         if(other != null && other.bodyType != RigidbodyType2D.Static)
         {
+            if (clip != null && source != null)
+            {
+                source.clip = clip;
+                source.Play();
+            }
+            else Debug.Log("FAIL");
+
             if (collision.GetComponent<TimeZone>())
             {
                 Destroy(other.gameObject);
             }
-            // other.isKinematic = true;
 
-         //   other.MovePosition(Destination.Exit.position);
-            //  other.isKinematic = false ;
             var relPoint = transform.InverseTransformPoint(other.transform.position);
             var relVelocity = -transform.InverseTransformDirection(other.velocity);
             other.velocity = Destination.transform.TransformDirection(relVelocity);
             other.MovePosition ( Destination.transform.TransformPoint(relPoint) + (convert2to3(other.velocity. normalized) * TeleportOffset));
-
-            // other.transform.rotation = Destination.rotation;
         }
 
     }
