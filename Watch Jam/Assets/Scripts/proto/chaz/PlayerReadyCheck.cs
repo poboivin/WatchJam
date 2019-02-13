45bcb8d2dbc74f4c0fb52ddbfbea39191e6e8946
Chaz Mossanen <chazmossanen@gmail.com>
2019-02-13 03:48:01 -0500

    Players can ready up by all pressing the 'Y' button.
    Character that player selected will not save over to new scene though.

---
+++ Watch Jam/Assets/Scripts/proto/chaz/PlayerReadyCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerReadyCheck : MonoBehaviour
+{
+	GameObject[] players;
+	int playersReady = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		players = GameObject.FindGameObjectsWithTag ("Player");
+
+		for (int i = 0; i < players.Length; i++) {
+			if (players [i].GetComponentInChildren<ReadyUp> ().isReady) {
+				playersReady += 1;
+
+			}
+			//Debug.Log (playersReady);
+
+		}
+
+		if (players.Length >= 2 && playersReady == players.Length) {
+			Debug.Log ("All PLayers Ready");
+			SceneManager.LoadScene ("ChazScene2");
+		}
+		else {
+			playersReady = 0;
+		}
+    }
+}
