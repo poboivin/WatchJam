d6ff73ecd5985b2e5f54173cd868c490277233ae
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2019-02-18 21:25:13 -0500

    Added Petals and a trigger to make them play need to set it up in julians scene.

---
+++ Watch Jam/Assets/Scripts/proto/Hunter/PetalPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetalPlayer : MonoBehaviour
+{
+    Animator PetalAnimator;
+    [SerializeField]
+    AnimationClip  PetalAnimClip;
+    bool stillPlaying=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PetalAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("overlap");
+        if (!stillPlaying)
+        {
+            StartCoroutine(WaitForEndOfAnimation());
+        }
+    }
+
+
+    private void OnTriggeEnter2D(Collider other)
+    {
+   
+    }
+
+
+
+    
+
+  
+
+
+
+
+
+
+    IEnumerator WaitForEndOfAnimation()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        stillPlaying = true;
+        PetalAnimator.Play("PetalAnim");
+
+        yield return new WaitForSeconds(PetalAnimClip.length);
+        GetComponent<SpriteRenderer>().enabled = false;
+        stillPlaying = false;
+    }
+
+}
+
