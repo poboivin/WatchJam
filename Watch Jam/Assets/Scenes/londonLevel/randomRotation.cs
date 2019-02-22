91f1809eef80eab6c347596a32094e485158f7d4
rasterradio <funnyguy150@gmail.com>
2019-02-22 18:00:22 -0500

    rotating clock hand

---
+++ Watch Jam/Assets/Scenes/londonLevel/randomRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomRotation : MonoBehaviour
+{
+    public float speed;
+    public int rotateDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateDir = Random.Range(0, 2);
+        speed = Random.Range(20f, 41f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotateDir == 0)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Watch Jam/Assets/Scenes/londonLevel/rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    private float speed = 0f;
+    public float timeToCompleteCircle = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = (Mathf.PI * 2) / timeToCompleteCircle;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+
+        //transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+        
+    }
+}
