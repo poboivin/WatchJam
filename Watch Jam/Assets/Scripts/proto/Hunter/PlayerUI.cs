000a4d1b7766754fe51f9728fbf3cf0f194b5741
Hunter Fraser <hunterfraser.gamedev@gmail.com>
2019-03-21 15:47:34 -0400

    Ui in progress

---
+++ Watch Jam/Assets/Scripts/proto/Hunter/PlayerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public GameObject OwnedUI;
+    public Image HealthBar;
+    public LifeSpan Health;
+    public Ammo AmmoAmount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OwnedUI.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
