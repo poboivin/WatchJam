b0727ea121cfd127c1f1d0623e15417aa440357a
hard-coded <cozyncoze@gmail.com>
2019-04-10 09:14:05 -0400

    changed a fire boosting to burst fire

--- Watch Jam/Assets/Scripts/proto/pickup/GunPowerFiringBoost.cs
+++ Watch Jam/Assets/Scripts/proto/pickup/GunPowerFiringBoost.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEditor;
 
 class GunPowerFiringBoost : GunPowerGeneric
 {
     private float oldFireRate;
     private float newFireRate;
+    bool startFiring = false;
+    GameObject bulletPrefab;
+    Vector3 firingPosition;
+    Quaternion firingRotation;
+    Vector2 bulletVelocity;
+    float firingTimer;
 
     public GunPowerFiringBoost( Gun playerGun, PickupFireBoost shotInfo )
         : base( playerGun, shotInfo.boostedBulletCount )
@@ -21,6 +29,7 @@ class GunPowerFiringBoost : GunPowerGeneric
         base.Activate();
         gun.fireRate = newFireRate;
         gun.SpecialBar.ToggleRapidFireBar( true );
+        Debug.Log( "Activate" );
     }
 
     public override void Deactivate()
@@ -28,6 +37,7 @@ class GunPowerFiringBoost : GunPowerGeneric
         base.Deactivate();
         gun.fireRate = oldFireRate;
         gun.SpecialBar.ToggleRapidFireBar( false );
+        Debug.Log( "Deactivate" );
     }
 
     public override void Update()
@@ -35,7 +45,42 @@ class GunPowerFiringBoost : GunPowerGeneric
         if( enableAbility )
         {
             gun.SpecialBar.SetRapidBarFill( numSpecialBullets );
+
+            if( startFiring )
+            {
+                firingTimer += Time.deltaTime;
+                if( firingTimer > newFireRate )
+                {
+                    GameObject newBullet = UnityEngine.Object.Instantiate( bulletPrefab, gun.gameObject.transform.position, firingRotation );
+                    if( newBullet )
+                    {
+                        bulletPrefab = newBullet;
+                        newBullet.GetComponent<Rigidbody2D>().velocity = bulletVelocity;
+                    }
+                    if( --numSpecialBullets == 0 )
+                    {
+                        enableAbility = false;
+                        startFiring = false;
+                    }
+                    firingTimer = 0.0f;
+                }
+            }
         }
     }
+
+    public override void FireBullet( GameObject bullet )
+    {
+        if( enableAbility )
+        {
+            numSpecialBullets--;
+            bulletPrefab = bullet;
+            bulletVelocity = bullet.GetComponent<Rigidbody2D>().velocity;
+            firingPosition = bullet.transform.position;
+            firingRotation = bullet.transform.rotation;
+            startFiring = true;
+            firingTimer = 0.0f;
+        }
+    }
+    
 }
 
